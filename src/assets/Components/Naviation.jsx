71ac9e8b47fd81293f5fe751f859048caf912b4a
StinaSteves <stina.steves@dci-student.org>
2024-11-29 03:43:43 +0100

    responsive

--- src/assets/Components/Naviation.jsx
+++ src/assets/Components/Naviation.jsx
@@ -13,7 +13,6 @@ export default function Navigation (){
             <ul>
             <li><a href="/">Home</a></li>
               <li><a href="#">About</a></li>
-              <li><a href="#">Shop</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
           </nav>
--- src/assets/Components/ShopCart.jsx
+++ src/assets/Components/ShopCart.jsx
@@ -1,8 +1,7 @@
+import PropTypes from 'prop-types'; 
 import './ShopCart.css';
 
-
 export default function ShopCart({ cartItems }) {
-  // Berechnung des Gesamtpreises
   const totalPrice = cartItems.reduce(
     (total, item) => total + item.quantity * parseFloat(item.price),
     0
@@ -10,32 +9,44 @@ export default function ShopCart({ cartItems }) {
 
   return (
     <div className='cartBackround'>
-    <div className="cart">
-      <h2>Your Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        <div>
-          <ul>
-            {cartItems.map((item, index) => (
-              <li key={index} className="cart-item">
+      <div className="cart">
+        <h2>Your Shopping Cart</h2>
+        {cartItems.length === 0 ? (
+          <p>Your cart is empty</p>
+        ) : (
+          <div>
+            <ul>
+              {cartItems.map((item, index) => (
+                <li key={index} className="cart-item">
                   <div className="cart-item-image">
-                <img src={item.image} alt={item.title} />
-              </div>
-              <div className="cart-item-details">
-                <p>{item.title} - {item.quantity} x {item.price}€</p>
-              </div>
-              </li>
-            ))}
-          </ul>
-          {/* Anzeige des Gesamtpreises */}
-          <div className="cart-total">
-            <h3>Total: {totalPrice.toFixed(2)}€</h3>
+                    <img src={item.image} alt={item.title} />
+                  </div>
+                  <div className="cart-item-details">
+                    <p>{item.title} - {item.quantity} x {item.price}€</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <div className="cart-total">
+              <h3>Total: {totalPrice.toFixed(2)}€</h3>
+            </div>
+            <button className='buyBtn'>pay now</button>
           </div>
-          <button className='buyBtn'>pay now</button>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
     </div>
   );
 }
+
+
+ShopCart.propTypes = {
+  cartItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired, 
+      quantity: PropTypes.number.isRequired, 
+      price: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        .isRequired, 
+      image: PropTypes.string, 
+    })
+  ).isRequired,
+};
